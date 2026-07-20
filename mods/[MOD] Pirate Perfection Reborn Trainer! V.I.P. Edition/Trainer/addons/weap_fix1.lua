4c7199839d1177a36f53cee59d3f9f087b807683
nooo-rgb <73847213+nooo-rgb@users.noreply.github.com>
2026-07-20 13:17:02 -0500

    Guard weapon timer setup during heist entry

--- mods/[MOD] Pirate Perfection Reborn Trainer! V.I.P. Edition/Trainer/addons/weap_fix1.lua
+++ mods/[MOD] Pirate Perfection Reborn Trainer! V.I.P. Edition/Trainer/addons/weap_fix1.lua
@@ -1,19 +1,35 @@
---Fixes problems, related to weapon preloading.
---Author: ThisJazzman
-backuper:backup('NewRaycastWeaponBase.set_timer')
+-- Fixes problems related to weapon preloading.
+-- Author: ThisJazzman
+
+backuper:backup("NewRaycastWeaponBase.set_timer")
+
 local alive = alive
 local pairs = pairs
+local type = type
 local NewRaycastWeaponBase = NewRaycastWeaponBase
-local super_set_timer = NewRaycastWeaponBase.super.set_timer
-NewRaycastWeaponBase.set_timer = function( self, ... )
-	if ( alive(self._unit) ) then --Check if we have unit
-		super_set_timer( self, ... )
-		for _,data in pairs( self._parts ) do
-			local unit = data.unit
-			if ( alive(unit) ) then --Check if part's data have unit
-				unit:set_timer( ... )
-				unit:set_animation_timer( ... )
-			end
+local super = NewRaycastWeaponBase.super
+local super_set_timer = super and super.set_timer
+
+NewRaycastWeaponBase.set_timer = function(self, ...)
+	if not alive(self._unit) then
+		return
+	end
+
+	if super_set_timer then
+		super_set_timer(self, ...)
+	end
+
+	-- set_timer can run before weapon assembly has populated _parts. Calling
+	-- pairs(nil) here used to crash while entering or dropping into a heist.
+	if not self._assembly_complete or type(self._parts) ~= "table" then
+		return
+	end
+
+	for _, data in pairs(self._parts) do
+		local unit = data and data.unit
+		if alive(unit) then
+			unit:set_timer(...)
+			unit:set_animation_timer(...)
 		end
 	end
 end
